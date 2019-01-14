9b9e4a39ea1269b03abbcaf560f597159f75d40d
wangyu <wyu@yitong.com.cn>
2019-01-14 14:29:34 +0800

    no message

---
+++ SourcetreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourcetreeTest
+//
+//  Created by 王俣 on 2019/1/14.
+//  Copyright © 2019 王俣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
